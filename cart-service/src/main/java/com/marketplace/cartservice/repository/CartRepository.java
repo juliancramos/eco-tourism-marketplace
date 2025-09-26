1c5ab6ae6124e099b3b476a3efb45c34877e01aa
Sergio Asencio <sergioasenciorodriguez@gmail.com>
2025-09-26 16:27:06 -0500

    feat(mapper - repository): Add mappers and repositories for new entities

--- cart-service/src/main/java/com/marketplace/cartservice/repository/CartRepository.java
+++ cart-service/src/main/java/com/marketplace/cartservice/repository/CartRepository.java
@@ -1,11 +1,9 @@
 package com.marketplace.cartservice.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marketplace.cartservice.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findByUserId(Long userId);
+    Cart findByUserId(Long userId);
 }
