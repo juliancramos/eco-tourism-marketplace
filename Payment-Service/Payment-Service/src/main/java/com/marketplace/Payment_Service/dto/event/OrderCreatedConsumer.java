51011e064a529cfacc9fab52d7ef5e65d4e54aea
SantiagoP67 <challengeboys399@gmail.com>
2025-09-29 15:40:46 -0500

    feat: fix consumer issues again

--- Payment-Service/Payment-Service/src/main/java/com/marketplace/Payment_Service/dto/event/OrderCreatedConsumer.java
+++ Payment-Service/Payment-Service/src/main/java/com/marketplace/Payment_Service/dto/event/OrderCreatedConsumer.java
@@ -1,5 +1,6 @@
 package com.marketplace.Payment_Service.dto.event;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderCreatedConsumer {
     private final PaymentService paymentService;
 
-    @KafkaListener(topics = "payments.request", groupId = "payment-service-group")
-    public void consumer(OrderDTO orderDTO) {
-        try {
-            log.info("Received message for order ID: {}", orderDTO.id());
-            paymentService.createPayment(orderDTO);
-            log.info("Processed payment for order ID: {}", orderDTO.id());
-        } catch (Exception e) {
-            log.error("Error processing message: {}", e.getMessage(), e);
-        }
-    }       
+    @Bean
+    public java.util.function.Consumer<OrderDTO> paymentsRequest() {
+        return orderDTO -> {
+            try {
+                log.info("Received message for order ID: {}", orderDTO.id());
+                paymentService.createPayment(orderDTO);
+                log.info("Processed payment for order ID: {}", orderDTO.id());
+            } catch (Exception e) {
+                log.error("Error processing message: {}", e.getMessage(), e);
+            }
+        };
+    }     
 }
