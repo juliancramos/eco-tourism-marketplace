2ba27dfdc0ac93dc30bbc38f6f6eaff72a399578
Julian Ramos <julianc.ramos21@gmail.com>
2025-09-30 13:34:21 -0500

    feat(service-catalog): check provider id before creating a new service

--- service-catalog/src/main/java/com/marketplace/servicecatalog/service/impl/ServiceCatalogServiceImpl.java
+++ service-catalog/src/main/java/com/marketplace/servicecatalog/service/impl/ServiceCatalogServiceImpl.java
@@ -16,10 +16,12 @@ import com.marketplace.servicecatalog.dto.UpdateServiceDTO;
 import com.marketplace.servicecatalog.mapper.ServiceMapper;
 import com.marketplace.servicecatalog.model.ServiceCategory;
 import com.marketplace.servicecatalog.model.ServiceEntity;
+import com.marketplace.servicecatalog.repository.ProviderCacheRepository;
 import com.marketplace.servicecatalog.repository.ServiceCategoryRepository;
 import com.marketplace.servicecatalog.repository.ServiceRepository;
 import com.marketplace.servicecatalog.service.ServiceCatalogService;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -28,6 +30,7 @@ public class ServiceCatalogServiceImpl implements ServiceCatalogService {
 
     private final ServiceRepository serviceRepository;
     private final ServiceCategoryRepository categoryRepository;
+    private final ProviderCacheRepository providerCacheRepository;
 
     @Override
     @Transactional
@@ -80,8 +83,13 @@ public class ServiceCatalogServiceImpl implements ServiceCatalogService {
 
     @Override
     public ServiceDTO create(CreateServiceDTO dto) {
+
+        // check providerId
+        if (!providerCacheRepository.existsById(dto.providerId())) {
+            throw new EntityNotFoundException("Provider not found with id: " + dto.providerId());
+        }
+
         var e = new ServiceEntity();
-        Long newId = System.currentTimeMillis();
         ServiceMapper.applyCreate(e, dto, LocalDateTime.now());
         e = serviceRepository.save(e);
         return ServiceMapper.toDto(e);
