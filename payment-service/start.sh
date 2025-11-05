52ba3978929b8af2cbfe58b8d13790e76a79bf13
Julian Ramos <julianc.ramos21@gmail.com>
2025-11-04 19:22:12 -0500

    chore(payment-service): dockerice microservice

---
+++ payment-service/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Esperando a Config Server en config-server:8888..."
+while ! nc -z config-server 8888; do
+  sleep 1
+done
+
+echo "Esperando a Eureka Server en eureka-server:8761..."
+while ! nc -z eureka-server 8761; do
+  sleep 1
+done
+
+echo "Iniciando payment-service..."
+java -jar app.jar
+
+
